2f1b5b682b2fba42cb644eb14d65ba8ff7e11993
Maria <mariam9010@gmail.com>
2016-08-06 13:41:44 +0300

    conversion1

--- conversion/simple.js
+++ conversion/simple.js
@@ -1,13 +1,16 @@
 // 
 var value =  10; 
 
+var convert = itoa1;
+
 var result = convert(value);
 
 console.log('given: ' + value + ' (' + typeof(value) + ')');
 
 console.log('result: ' + result + ' (' + typeof(result) + ')');
 
-function convert(value) {
-    return value;
+function itoa1(value) {
+    return '' + value;
 }
 
+//string  + b = string
