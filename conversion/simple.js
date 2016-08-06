91d3c8aa85f55d9394bd9a71e42e43ad0555f658
Maria <mariam9010@gmail.com>
2016-08-06 13:22:51 +0300

    convertion empty

---
+++ conversion/simple.js
@@ -0,0 +1,13 @@
+// 
+var value =  10; 
+
+var result = convert(value);
+
+console.log('given: ' + value + ' (' + typeof(value) + ')');
+
+console.log('result: ' + result + ' (' + typeof(result) + ')');
+
+function convert(value) {
+    return value;
+}
+
