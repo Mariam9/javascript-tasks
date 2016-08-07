c0c18e9d81190fe6ef6abfe61073d8942570728f
Maria <mariam9010@gmail.com>
2016-08-08 00:16:22 +0300

    foreach

--- conversion/simple.js
+++ conversion/simple.js
@@ -4,6 +4,14 @@ var tasks = [
         description: 'convert number to string using concat',
         converter: itoa1, 
         input: 10
+    },
+
+    {
+        description: '',
+        converter: function() {
+            return 0;
+        },
+        input: null
     }
 ];
 
@@ -13,15 +21,25 @@ function itoa1(value) {
     return '' + value;
 }
 
-for ( var i = 0; i < tasks.length; i++) {
-    callback(i, tasks[i]);
+//  ctr k c, ctrl k u
+tasks.forEach(function(i, e, arr) {
+    callback(i, e);
+});
 
-}
+//forEach(tasks, callback);
 
 function callback(i, e) {
    var value =  e.input; 
    var result = e.converter(value);
+   console.log(e.description);
    console.log('given: ' + value + ' (' + typeof(value) + ')');
    console.log('result: ' + result + ' (' + typeof(result) + ')');
-   console.log(e.description)
+   console.log('---------------------------------------------');
 }
+
+function forEach(array, func) { //некий массив, некая функция, аргументі, потому что мы не знаем
+    for (var i = 0; i < array.length; i++) {
+        func(i, array[i]);
+    }
+}
+
